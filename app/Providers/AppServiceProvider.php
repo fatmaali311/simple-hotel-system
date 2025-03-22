6c2c6b8dfaad9a6f524f43094e5bd28b0a1c26c6
Ghada Emad <ghadaemad011@gmail.com>
2025-03-22 21:16:22 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\View;
+
 use Illuminate\Support\ServiceProvider;
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Auth;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,21 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< HEAD
-        Inertia::share([
-            'auth' => [
-                'user' => Auth::check() ? [
-                    'id' => Auth::id(),
-                    'permissions' => Auth::user()->permissions->pluck('name')->toArray(), // Fetch permissions correctly
-
-                ] : null
-            ]
-        ]);
-        Log::info('Authenticated User:', ['user' => Auth::user()]);
+        //
     }
-   
-=======
-       }
-
->>>>>>> c249f7927bcd1c14590fe12e06622c4db3fad1cf
 }
